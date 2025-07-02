4824fa1534407a34eee9ece88ca98c3cfe24add4
ayaz1017 <your-email@example.com>
2025-07-02 21:50:45 +0530

    Google

--- Back-End/app.js
+++ Back-End/app.js
@@ -39,7 +39,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/user/auth/google/callback",
+      callbackURL: "https://shopnests.onrender.com/api/user/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
